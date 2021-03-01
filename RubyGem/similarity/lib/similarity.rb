78458193f7809b1969e51ace20859127673e4b7c
Luis Hilario <luishilariomtz@gmail.com>
2021-03-01 10:33:32 -0600

    Gem Functionality

--- RubyGem/similarity/lib/similarity.rb
+++ RubyGem/similarity/lib/similarity.rb
@@ -1,6 +1,10 @@
 require "similarity/version"
+require "similarity/levenshtein"
 
 module Similarity
   class Error < StandardError; end
   # Your code goes here...
+  class String 
+    include Levenshtein #Monkey Patch
+  end
 end
---
+++ RubyGem/similarity/lib/similarity/levenshtein.rb
@@ -0,0 +1,31 @@
+module Levenshtein
+
+    def distance_to(second)
+        return [self.length, second.length].max if [self.length, second.length].min == 0
+
+        return levenshtein(first: self, second: second) unless block_given?
+        yield self,second
+    end
+
+    def levenshtein(first:, second:)
+        matrix = [(0..first.length).to_a]
+
+        (1..second.length).each do |j|
+            matrix << [j] + [0] * (first.length)
+        end
+        (1..second.length).each do |i|
+            (1..first.length).each do |j|
+                if first[j-1] == second[i-1]
+                    matrix[i][j] = matrix[i-1][j-1]
+                else
+                    matrix[i][j] = [
+                        matrix[i-1][j],
+                        matrix[i][j-1],
+                        matrix[i-1][j-1],
+                    ].min + 1
+                end
+            end
+        end
+        return matrix.last.last
+    end
+end
